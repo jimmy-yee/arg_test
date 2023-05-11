cebfd4ebc7f0073dc257551749b1fc9e4cee4730
jimmy-yee <81695313+jimmy-yee@users.noreply.github.com>
2023-05-12 04:11:42 +0800

    test button

---
+++ ar.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('#refresh-button');
+
+
+function turnpage(){
+    window.open("https://www.w3schools.com");
+}
+
+
+
+
+button.addEventListener('click', turnpage())
